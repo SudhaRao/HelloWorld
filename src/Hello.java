ffb34dae227d4bf8959a01222f13fa11442bc531
sudha <hrsudha@gmail.com>
2015-09-28 08:45:45 -0500

    New line of code added.

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World - New");
-
+		System.out.println("New line to test the new checkin to git remote repository");
 	}
 
 }
+
