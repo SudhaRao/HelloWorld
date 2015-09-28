9997a6e3763c360219b7a83122ca4809a67e2121
SudhaRao <hrsudha@gmail.com>
2015-09-28 08:53:35 -0500

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World - New");
 		System.out.println("New line to test the new checkin to git remote repository");
+		System.out.println("Another line to test the new checkout from git to local repository");
 	}
 
 }
